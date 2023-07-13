36f13024258f6d84199e5185893ed52affdefce0
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-13 21:58:30 +0200

    push-release-final-version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 
 //imposto il giorno cui si deve arrivare in ms
 const countdownDate = new Date(2023, 7, 14, 9, 30).getTime();
@@ -9,15 +10,20 @@ console.log(countdownDate);
 let mainTimer = setInterval(function(){    
     const nowDate = new Date().getTime();
     const millDiff = countdownDate - nowDate;
+    //conversione di ms in hr, min, sec
     let hours = Math.floor((millDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((millDiff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((millDiff % (1000 * 60)) / 1000);
     visibleTimer.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+    // condizione che blocca il timer una volta raggiunto lo 0
     if (millDiff < 0 ){
         clearInterval(mainTimer);
         isibleTimer.innerHTML = "LET'S START";
     }
 } , 100);
+
+
 
 
 
