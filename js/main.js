3d25fb53d802dded14db7261017053ea740deb2f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-13 15:02:18 +0200

    choose-bg

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 //imposto il giorno cui si deve arrivare in ms
 const countdownDate = new Date(2023, 7, 14, 9, 30).getTime();
-const visibleTimer = document.getElementById("timer")
-console.log(countdownDate)
+const visibleTimer = document.getElementById("timer");
+console.log(countdownDate);
 
 
 
@@ -12,7 +12,7 @@ setInterval(function(){
     let hours = Math.floor((millDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((millDiff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((millDiff % (1000 * 60)) / 1000);
-    visibleTimer.innerHTML = `${hours}:${minutes}:${seconds} `
+    visibleTimer.innerHTML = `${hours}:${minutes}:${seconds}; `
 } , 100);
 
 
