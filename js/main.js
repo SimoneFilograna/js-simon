68f988f5ab3a9c71d2241f2979a4d11195c61808
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-14 09:52:46 +0200

    fixed-mese

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 "use strict"
 
 //imposto il giorno cui si deve arrivare in ms
-const countdownDate = new Date(2023, 7, 14, 9, 30).getTime();
+const countdownDate = new Date(2023, 6, 14, 9, 30).getTime();
+
 const visibleTimer = document.getElementById("timer");
 console.log(countdownDate);
 
@@ -15,13 +16,13 @@ let mainTimer = setInterval(function(){
     let minutes = Math.floor((millDiff % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((millDiff % (1000 * 60)) / 1000);
     visibleTimer.innerHTML = `${hours}:${minutes}:${seconds}`;
-
     // condizione che blocca il timer una volta raggiunto lo 0
     if (millDiff < 0 ){
         clearInterval(mainTimer);
-        isibleTimer.innerHTML = "LET'S START";
+        visibleTimer.innerHTML = "LET'S START";
     }
 } , 100);
+
 
 
 
